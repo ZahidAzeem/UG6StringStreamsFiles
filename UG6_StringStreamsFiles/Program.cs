affc178be7a220da54ac28de8c24ac98b48f8afe
ZahidAzeem <azeemior@gmail.com>
2021-09-28 22:09:30 +0200

    Add project files.

---
+++ UG6_StringStreamsFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UG6_StringStreamsFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
